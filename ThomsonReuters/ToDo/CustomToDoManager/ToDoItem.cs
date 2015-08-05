56f33d4d9a56d5f2391bffc2491dd11c4ded7b2d
nikkishkin <nikkishkin@ya.ru>
2015-08-05 13:15:02 +0300

    Workshop

---
+++ ThomsonReuters/ToDo/CustomToDoManager/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using ToDo.Infrastructure;
+
+namespace CustomToDoManager
+{
+    class CustomToDoItem : IToDoItem
+    {
+        public int ToDoId { get; set; }
+        public int UserId { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ThomsonReuters/ToDo/CustomToDoManager/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomToDoManager
+{
+    public class User
+    {
+        public User()
+        {
+            toDoList = new List<Item>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Item> toDoList { get; set; }
+
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+}
